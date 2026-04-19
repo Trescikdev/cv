eb35ecab30290fd620fa7a3bdf70623449ad428a
Trescikdev <sutorasam@gmail.com>
2026-04-19 17:58:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php include('lang.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $texts['title']; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+<body>
+
+<div class="container mt-4 d-flex justify-content-end">
+    <div class="lang-switcher d-flex align-items-center">
+        <a href="?lang=en" class="<?php echo ($lang == 'en') ? 'active-flag' : 'inactive-flag'; ?>" title="English">
+            <img src="https://flagcdn.com/w40/gb.png" 
+                 srcset="https://flagcdn.com/w80/gb.png 2x" 
+                 width="30" 
+                 alt="English" 
+                 style="cursor: pointer;">
+        </a>
+        
+        <span class="mx-2 text-muted">|</span>
+        
+        <a href="?lang=sk" class="<?php echo ($lang == 'sk') ? 'active-flag' : 'inactive-flag'; ?>" title="Slovensky">
+            <img src="https://flagcdn.com/w40/sk.png" 
+                 srcset="https://flagcdn.com/w80/sk.png 2x" 
+                 width="30" 
+                 alt="Slovensky" 
+                 style="cursor: pointer;">
+        </a>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-11 cv-container">
+            
+            <div class="row">
+                <div class="col-md-4 border-end pe-md-4">
+                    <div class="text-center mb-4">
+                        <div class="profile-photo-frame">
+                            <img src="cover-photo.jpg" alt="Samuel Šútora" class="img-fluid">
+                        </div>
+                    </div>
+                    
+                    <section>
+                        <h2 class="section-title"><?php echo $texts['about_me']; ?></h2>
+                        <p class="small"><?php echo $texts['about_me_desc']; ?></p>
+                    </section>
+
+                    <section class="mb-4">
+                        <h2 class="section-title"><?php echo $texts['links']; ?></h2>
+                        <div class="contact-info">
+                        <div class="contact-item mb-1">
+                            <i class="bi bi-geo-alt-fill"></i>
+                            <span><?php echo $texts['residence']; ?></span>
+                        </div>
+                        <i class="bi bi-envelope-fill"></i>
+                        <a href="mailto:sutorasam@gmail.com" class="contact-item">
+                            <span>sutorasam@gmail.com</span>
+                        </a>
+                        <br>
+                        <i class="bi bi-telephone-fill"></i>
+                        <a href="tel:+421907407116" class="contact-item">
+                            <span>+421 907 407 116</span>
+                        </a>
+                        </div>
+                        <div class="contact-links">
+                            <a href="https://www.linkedin.com/in/samuel-šútora-917165368/" class="d-block mb-1 text-decoration-none"><i class="bi bi-linkedin me-2"></i>LinkedIn</a>
+                            <a href="#" class="d-block mb-1 text-decoration-none"><i class="bi bi-github me-2"></i>GitHub</a>
+                        </div>
+                    </section>
+                </div>
+
+                <div class="col-md-8 ps-md-4">
+                    <header class="text-md-start text-center mb-5">
+                        <h1 class="display-4 fw-bold">Samuel Šútora</h1>
+                        <p class="lead text-primary"><?php echo $texts['last_updated']; ?>APR 2026</p>
+                    </header>
+
+                    <section class="mb-4">
+                        <h2 class="section-title"><?php echo $texts['summary']; ?></h2>
+                        <p>...</p>
+                    </section>
+
+                    <section class="mb-4">
+                        <h2 class="section-title"><?php echo $texts['experience']; ?></h2>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0 fw-bold">Web admin</h5>
+                            <span class="job-date text-uppercase">Sep 2025 – <?php echo $texts['present']; ?></span>
+                        </div>
+                        <div class="text-primary mb-2 green">Hulman.sk</div>
+                        <ul>
+                            <li>HTML/CSS <?php echo $texts['website_maintenance']; ?>.</li>
+                            <li><?php echo $texts['occasional_php_development']; ?>.</li>
+                        </ul>
+                        <!-- <button class="btn btn-secondary btn-sm"><?php echo $texts['more_info']; ?></button> -->
+
+                        <div><br></div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0 fw-bold"><?php echo $texts['job_title_1']; ?></h5>
+                            <span class="job-date text-uppercase">SEP 2022 – JUL 2023</span>
+                        </div>
+                        <div class="text-primary mb-2">FBC Grasshoppers AC UNIZA Žilina</div>
+                        <p><?php echo $texts['job_summary_1']; ?>.</p>
+                        <!-- <button class="btn btn-secondary btn-sm"><?php echo $texts['more_info']; ?></button> -->
+                    </section>
+
+                    <section class="mb-4">
+                        <h2 class="section-title"><?php echo $texts['education']; ?></h2>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0 fw-bold">Masarykova Univerzita</h5>
+                            <span class="job-date text-uppercase">Sep 2024 – <?php echo $texts['present']; ?></span>
+                        </div>
+                        <div class="text-primary mb-2"><?php echo $texts['study_programme']; echo $texts['degree']; ?></div>
+
+                        <div><br></div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="mb-0 fw-bold">Gymnázium Varšavská cesta 1</h5>
+                            <span class="job-date text-uppercase">Sep 2016 – May 2024</span>
+                        </div>
+                        <div class="text-primary mb-2"><?php echo $texts['high_school_summary']; ?></div>
+                    </section>
+
+                    <section class="mb-4">
+                        <h2 class="section-title"><?php echo $texts['skills']; ?></h2>                    
+                        <div class="d-flex flex-wrap gap-2">
+                            <span class="skill-bubble tech-backend">Python</span>
+                            <span class="skill-bubble tech-backend">C</span>
+                            <span class="skill-bubble tech-backend">C#</span>
+                            <span class="skill-bubble">SQL</span>
+                            <span class="skill-bubble">HTML5</span>
+                            <span class="skill-bubble">CSS</span>
+                            <span class="skill-bubble tech-backend">PHP</span>
+                            <span class="skill-bubble tech-backend">Haskell</span>
+                        </div>
+                        <div><br></div>
+                        <div class="d-flex flex-wrap gap-2">
+                            <span class="skill-bubble"><?php echo $texts['english']; ?></span>
+                            <span class="skill-bubble">MS Office</span>
+                            <span class="skill-bubble">Github</span>
+                        </div>
+                    </section>
+
+                    <section class="mb-4">
+                        <h2 class="section-title"><?php echo $texts['hobbies_title'] ?></h2>
+                        <p><?php echo $texts['hobbies_desc']; ?></p>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ lang.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+
+// Check if a language is set in the URL, otherwise check session, default to English
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+    $_SESSION['lang'] = $lang;
+} elseif (isset($_SESSION['lang'])) {
+    $lang = $_SESSION['lang'];
+} else {
+    $lang = 'en';
+}
+
+$translations = [
+    'en' => [
+        'title' => 'Samuel Šútora CV',
+        'summary' => 'Professional Summary',
+        'experience' => 'Work Experience',
+        'skills' => 'Technical Skills',
+        'contact' => 'Contact Me',
+        'present' => 'present',
+        'website_maintenance' => 'website maintenance',
+        'occasional_php_development' => 'occasional PHP development',
+        'more_info' => 'more info v',
+        'education' => 'Education',
+        'links' => 'Important links',
+        'about_me' => 'About me',
+        'study_programme' => 'Programming and app development',
+        'degree' => ' (Bachelor\'s degree)',
+        'last_updated' => 'last updated: ',
+        'high_school_summary' => 'Main focus: languages, mathematics, IT',
+        'job_title_1' => 'Youth floorball coach',
+        'job_summary_1' => 'Coaching of children (5-12) - floorball and related sports activities',
+        'english' => 'English - C1',
+        'hobbies_title' => 'Hobbies and free time',
+        'about_me_desc' => 'I am a 21 year old CS student looking for na internship
+                            or a part time job. I am keen on meeting new people, 
+                            which comes naturally to me thanks to my flexibility
+                            and friendly nature. I am very comfortable working in a
+                            team and communication is a priority for me. Hoping to 
+                            become a productive developer by the time I finish my studies.',
+        'hobbies_desc' => 'Every day, I try to find time for sports, such as floorball, 
+                            basketball, running, or working out. I enjoy quizzes and puzzles,
+                            as well as board or computer games. However, my biggest passion is
+                            traveling, especially through Nordic countries. I also love listening
+                            to good music or watching a well-made movie or series.',
+        'residence' => 'Žilina, Slovakia & Brno, Czechia'
+    ],
+    'sk' => [
+        'title' => 'Samuel Šútora CV',
+        'summary' => 'Profesijný profil',
+        'experience' => 'Pracovné skúsenosti',
+        'skills' => 'Technické zručnosti',
+        'contact' => 'Kontaktujte ma',
+        'present' => 'Súčasnosť',
+        'website_maintenance' => 'údržba webstránok',
+        'occasional_php_development' => 'Občasný vývoj v PHP',
+        'more_info' => 'viac info v',
+        'education' => 'Vzdelanie',
+        'links' => 'dôležité odkazy & kontakt',
+        'about_me' => 'O mne',
+        'study_programme' => 'Programovanie a vývoj aplikácií',
+        'degree' => ' (Bakalársky titul)',
+        'last_updated' => 'naposledy upravené: ',
+        'high_school_summary' => 'Hlavné zameranie: jazyky, matematika, IT',
+        'job_title_1' => 'Tréner detí',
+        'job_summary_1' => 'Tréner detí (5-12) - flórbal a s tým spojené športové aktivity',
+        'english' => 'Angličtina - C1',
+        'hobbies_title' => 'Voľnočasové aktivity',
+        'about_me_desc' => 'Som 21-ročný študent informatiky, ktorý si hľadá stáž alebo
+                            prácu na čiastočný úväzok. Rád spoznávam nových ľudí, čo mi 
+                            vďaka mojej flexibilite a priateľskej povahe ide prirodzene.
+                            Veľmi dobre sa mi pracuje v tíme a komunikácia je pre mňa 
+                            prioritou. Verím, že do konca štúdia sa zo mňa stane produktívny
+                            vývojár.',
+        'hobbies_desc' => 'Každý deň sa snažím nájsť si chvíľu na šport ako napríklad flórbal, basketbal, behanie či posilovanie. Bavia ma kvízy a hádanky alebo stolné či počítačové hry. Moja najväčšia záľuba je však cestovanie, hlavne po severských krajinách. Vždy si rád pustím dobrú hudbu alebo pozriem podarený film/seriál.',
+        'residence' => 'Žilina/Brno'
+        ]
+];
+
+// Define a shorthand for the current language
+$texts = $translations[$lang];
+?>
